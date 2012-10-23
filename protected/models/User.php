845de7923361ae04aacf9c7fd4ac95eb224a5479
foxye <saltice@live.com>
2012-10-23 17:22:26 +0800

    check password
    Change-Id: I18c7192bdc922ac77b1a6180a9f5b8a190e4b3d0
    Signed-off-by: foxye <saltice@live.com>

--- protected/models/User.php
+++ protected/models/User.php
@@ -22,6 +22,9 @@ class User extends CActiveRecord
 	 * @param string $className active record class name.
 	 * @return User the static model class
 	 */
+	
+	var $itoa64;
+	
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
@@ -113,7 +116,82 @@ class User extends CActiveRecord
 		));
 	}
 	
-	public function validatePassword($password){
-		return true;
+	
+	public function checkpassword($password, $stored_hash)
+	{
+		$hash = $this->crypt_private($password, $stored_hash);
+		if ($hash[0] == '*')
+			$hash = crypt($password, $stored_hash);
+	
+		return $hash == $stored_hash;
+	}
+	
+	public function crypt_private($password, $setting)
+	{	
+		$this->itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+		$output = '*0';
+		if (substr($setting, 0, 2) == $output)
+			$output = '*1';
+	
+		$id = substr($setting, 0, 3);
+		# We use "$P$", phpBB3 uses "$H$" for the same thing
+		if ($id != '$P$' && $id != '$H$')
+			return $output;
+	
+		$count_log2 = strpos($this->itoa64, $setting[3]);
+		if ($count_log2 < 7 || $count_log2 > 30)
+			return $output;
+	
+		$count = 1 << $count_log2;
+	
+		$salt = substr($setting, 4, 8);
+		if (strlen($salt) != 8)
+			return $output;
+	
+		# We're kind of forced to use MD5 here since it's the only
+		# cryptographic primitive available in all versions of PHP
+		# currently in use.  To implement our own low-level crypto
+		# in PHP would result in much worse performance and
+		# consequently in lower iteration counts and hashes that are
+		# quicker to crack (by non-PHP code).
+		if (PHP_VERSION >= '5') {
+			$hash = md5($salt . $password, TRUE);
+			do {
+				$hash = md5($hash . $password, TRUE);
+			} while (--$count);
+		} else {
+			$hash = pack('H*', md5($salt . $password));
+			do {
+				$hash = pack('H*', md5($hash . $password));
+			} while (--$count);
+		}
+	
+		$output = substr($setting, 0, 12);
+		$output .= $this->encode64($hash, 16);
+	
+		return $output;
+	}
+	
+	function encode64($input, $count)
+	{
+		$output = '';
+		$i = 0;
+		do {
+			$value = ord($input[$i++]);
+			$output .= $this->itoa64[$value & 0x3f];
+			if ($i < $count)
+				$value |= ord($input[$i]) << 8;
+			$output .= $this->itoa64[($value >> 6) & 0x3f];
+			if ($i++ >= $count)
+				break;
+			if ($i < $count)
+				$value |= ord($input[$i]) << 16;
+			$output .= $this->itoa64[($value >> 12) & 0x3f];
+			if ($i++ >= $count)
+				break;
+			$output .= $this->itoa64[($value >> 18) & 0x3f];
+		} while ($i < $count);
+	
+		return $output;
 	}
 }
